e0a2e8a6e358398f13b94346d63ede10aa30178b
tatina140810 <tatu140810tatu@gmail.com>
2024-04-25 17:37:02 +0600

    Initial Commit

---
+++ NoteApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoteApp
+//
+//  Created by Tatina Dzhakypbekova on 25/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
