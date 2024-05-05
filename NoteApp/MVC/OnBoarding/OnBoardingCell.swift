b0b1ecc2c5802cd6dbea7e1eca2a8a38506cbe90
tatina140810 <tatu140810tatu@gmail.com>
2024-05-05 21:04:16 +0600

    fifth commit

---
+++ NoteApp/MVC/OnBoarding/OnBoardingCell.swift
@@ -0,0 +1,8 @@
+//
+//  OnBoardingCell.swift
+//  NoteApp
+//
+//  Created by Tatina Dzhakypbekova on 5/5/24.
+//
+
+import Foundation
---
+++ NoteApp/MVC/OnBoarding/OnBoardingView.swift
@@ -0,0 +1,8 @@
+//
+//  OnBoardingView.swift
+//  NoteApp
+//
+//  Created by Tatina Dzhakypbekova on 5/5/24.
+//
+
+import Foundation
